81b24d98145b70adfee545a27660b07f919a5fee
tfarhan00 <farhanalryan2@gmail.com>
2024-10-07 06:04:05 +0700

    feat(wallet): add wallet routes and fix messaging

--- config.js
+++ config.js
@@ -2,45 +2,48 @@ import { CHAIN } from "@prisma/client";
 
 export const CHAINS = [
   {
-    id: 'mainnet-beta',
-    dbChainId: CHAIN.MAINNET_BETA, // 'MAINNET_BETA' 
-    name: 'Mainnet Beta',
-    rpcUrl: process.env.MAINNET_RPC_URL || 'https://api.mainnet-beta.solana.com',
-    escrowProgramId: process.env.ESCROW_PROGRAM_ID
+    id: "mainnet-beta",
+    dbChainId: CHAIN.MAINNET_BETA, // 'MAINNET_BETA'
+    name: "Mainnet Beta",
+    rpcUrl:
+      process.env.MAINNET_RPC_URL || "https://api.mainnet-beta.solana.com",
+    escrowProgramId: process.env.ESCROW_PROGRAM_ID,
   },
   {
-    id: 'devnet',
+    id: "devnet",
     dbChainId: CHAIN.DEVNET, // 'DEVNET'
-    name: 'Devnet',
-    rpcUrl: process.env.DEVNET_RPC_URL || 'https://api.devnet.solana.com',
-    escrowProgramId: process.env.ESCROW_PROGRAM_ID
+    name: "Devnet",
+    rpcUrl: process.env.DEVNET_RPC_URL || "https://api.devnet.solana.com",
+    escrowProgramId: process.env.ESCROW_PROGRAM_ID,
   },
-  {
-    id: 'localnet',
-    dbChainId: CHAIN.LOCALNET, // 'LOCALNET'
-    name: 'Localnet',
-    rpcUrl: process.env.LOCALNET_RPC_URL || 'http://localhost:8899',
-    escrowProgramId: process.env.ESCROW_PROGRAM_ID
-  }
-]
+  // {
+  //   id: "localnet",
+  //   dbChainId: CHAIN.LOCALNET, // 'LOCALNET'
+  //   name: "Localnet",
+  //   rpcUrl: process.env.LOCALNET_RPC_URL || "http://localhost:8899",
+  //   escrowProgramId: process.env.ESCROW_PROGRAM_ID,
+  // },
+];
+
+console.log("chain", CHAINS);
 
 export const VESTING_CONFIG = [
   {
-    id: 'TIME',
-    name: 'Time-based Vesting',
-    conditions: ['1-month', '2-months', '3-months', '6-months', '1-year'],
-    allowCustom: false,  // Time-based vesting doesn't allow custom conditions
+    id: "TIME",
+    name: "Time-based Vesting",
+    conditions: ["1-month", "2-months", "3-months", "6-months", "1-year"],
+    allowCustom: false, // Time-based vesting doesn't allow custom conditions
   },
   {
-    id: 'MARKETCAP',
-    name: 'Market Cap-based Vesting',
-    conditions: [500_000, 1_000_000, 5_000_000, 10_000_000],  // Predefined thresholds
-    allowCustom: true,  // Market Cap-based vesting allows custom thresholds
+    id: "MARKETCAP",
+    name: "Market Cap-based Vesting",
+    conditions: [500_000, 1_000_000, 5_000_000, 10_000_000], // Predefined thresholds
+    allowCustom: true, // Market Cap-based vesting allows custom thresholds
   },
   {
-    id: 'NONE',
-    name: 'None Vesting',
-    conditions: null,  // No conditions for "None" type
-    allowCustom: false,  // No custom conditions allowed
+    id: "NONE",
+    name: "None Vesting",
+    conditions: null, // No conditions for "None" type
+    allowCustom: false, // No custom conditions allowed
   },
 ];
